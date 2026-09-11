4b6e2c9465d6f91718e2e1a35c59b394a402063f
Deception101 <kytenjohnson@sheridan.edu>
2026-09-11 04:20:59 +0000

    added addtrack function

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 struct MusicTrack {
     string title;
     string artist;
+    string album;
     int duration;
     string genre;
     int releaseYear;
@@ -44,8 +45,8 @@ int main() {
             case 2: displayLibrary(library); break;
             case 3: saveLibraryToFile(library,filename); break;
             case 4: loadLibraryFromFile(library,filename); break;
-            case 5: cout << "Existing Music Library. Goodbye\n"; break;
-            default: cout << "Invalid choice. Select a a valid option\n\n";
+            case 5: cout << "Exiting Music Library. Goodbye\n"; break;
+            default: cout << "Invalid choice. Select a valid option\n\n";
         }
 
 }
@@ -55,11 +56,38 @@ int main() {
 }
 
 void printMenu() {
-    cout << "==== usic Library Menu ====\n";
+    cout << "==== Music Library Menu ====\n";
     cout << "1. Add Track\n";
     cout << "2. Display Library\n";
     cout << "3. Save Library to File\n";
     cout << "4. Load Library from File\n";
     cout << "5. Exit\n";
     cout << "Enter your choice: " ;
+}
+
+void addTrack(vector<MusicTrack>& library) {
+    MusicTrack track;
+
+    cout << "Enter track title: ";
+    getline(cin,track.title);
+
+    cout << "Enter artist: ";
+    getline(cin,track.artist); 
+
+    cout << "Enter album: ";
+    getline(cin,track.album);
+
+    cout << "Enter duration (seconds): ";
+    cin >> track.duration;
+    cin.ignore(numeric_limits<streamsize>:: max(), '\n');
+
+    cout << "Enter genre: ";
+    getline(cin,track.genre);
+
+    cout << "Enter release year: ";
+    cin >> track.releaseYear;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    library.push_back(track);
+    cout << "Track added successfully.\n\n";
 }
