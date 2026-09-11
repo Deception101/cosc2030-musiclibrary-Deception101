8b311696fe9b611eebaa8ab73ea68cb611fd8132
Deception101 <kytenjohnson@sheridan.edu>
2026-09-11 03:15:33 +0000

    made musicTrack struct

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <limits>
 
 using namespace std;
 
+struct MusicTrack {
+    string title;
+    string artist;
+    int duration;
+    string genre;
+    int releaseYear;
+};
+
+void printMenu();
+void addTrack(vector< MusicTrack> & library);
+void displayLibrary(const vector<MusicTrack>&library);
+void saveLibraryToFile(const vector<MusicTrack>& library, const string& filename);
+void loadLibraryFromFile(vector<MusicTrack> & library, const string& filename);
+
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+    
     return 0;
 }
